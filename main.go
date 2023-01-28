4cca3f0804c761afadaaf8d1dfd84b946ef3aa09
Stefan Wintergerst <wintergerst.stefan@googlemail.com>
2023-01-28 02:27:55 +0100

    add http logging, add quiet option

--- main.go
+++ main.go
@@ -7,12 +7,30 @@ import (
 	"net/http"
 )
 
+func loggingMiddleware(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		log.Printf("[%s]: %s\n", r.Method, r.URL)
+		next.ServeHTTP(w, r)
+	})
+}
+
 func main() {
 	port := 6969
 	flag.IntVar(&port, "port", 6969, "")
+
+	quiet := false
+	flag.BoolVar(&quiet, "quiet", false, "")
+
 	flag.Parse()
+
 	host := fmt.Sprintf(":%d", port)
-	log.Printf("running on: '%s'\n", host)
-	http.Handle("/", http.FileServer(http.Dir(".")))
+
+	if quiet {
+		http.Handle("/", http.FileServer(http.Dir(".")))
+	} else {
+		log.Printf("running on: '%s'\n", host)
+		http.Handle("/", loggingMiddleware(http.FileServer(http.Dir("."))))
+	}
+
 	http.ListenAndServe(host, nil)
 }
